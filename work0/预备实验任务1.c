dd587284f36c03371b060beb0599ba3297cc436f
dbydd <1992003927@qq.com>
2023-09-21 13:42:45 +0800

    init

--- work0/预备实验任务1.c
+++ work0/预备实验任务1.c
@@ -22,6 +22,10 @@ int main() {
 	return 0;
 }
 
+/// @brief 取平均值，返回结果为double类型
+/// @param a 数组
+/// @param n 数组元素个数
+/// @return 平均值
 double getAvg(int a[], int n) {
 
 	double sum = 0;
@@ -32,9 +36,14 @@ double getAvg(int a[], int n) {
 	return sum / n;
 }
 
+/// @brief 获取 @param a 中最接近 @param x 的元素
+/// @param a 输入数组
+/// @param n 数组元素个数
+/// @param x 带查找值
+/// @return 查询到的元素下标
 int getIndex(int a[], int n, double x) {
 	int index = 0;
-	double err = abs([0] - x);
+	double err = abs(a[0] - x);
 	for (int i = 1; i < n; i++) {
 		double temp = abs(a[i] - x);
 		if (temp < err) {
@@ -46,3 +55,10 @@ int getIndex(int a[], int n, double x) {
 
 	return index;
 }
+
+1 7 4 0 9 4 8 8
+avg = 5.125000
+minpos = 1, value = 7
+(base)
+# 19920 @ dbydd_laptop in ~[13:25 : 41]
+$
--- work0/预备实验任务2.c
+++ work0/预备实验任务2.c
@@ -1,5 +1,7 @@
 #include "stdio.h"
 #include "stdlib.h"
+
+/// @brief 似乎是系统编码问题，源文件的非ascii字符产生了乱码
 typedef struct {
     int xh;   /*ѧ��*/
     float sg; /*����*/
@@ -7,6 +9,7 @@ typedef struct {
 int getRecs(STUDENTS s[]);
 void sort(STUDENTS s[], int n);
 
+/// @brief 比较函数，参见 sort 函数
 int compare(STUDENTS* a, STUDENTS* b) {
     return (*a).sg >= (*b).sg;
 }
@@ -16,12 +19,12 @@ int main() {
     STUDENTS stu[20];
     int n, i;
     n = getRecs(stu);
-    // for (i = 0; i < n; i++) {
-        // printf("%d %.2f\n", stu[i].xh, stu[i].sg);
-    // }
+    for (i = 0; i < n; i++) {
+        printf("%d %.2f\n", stu[i].xh, stu[i].sg);
+    }
 
     sort(stu, n);
-    printf("\n���������\n");
+    printf("\n学号\t身高\n");
     for (i = 0; i < n; i++) {
         printf("%d %.2f\n", stu[i].xh, stu[i].sg);
     }
@@ -29,19 +32,21 @@ int main() {
 }
 
 void sort(STUDENTS a[], int n) {
+    //c std标准库自带的快速排序算法
+    //参数格式为：输入数组，数组大小，数组元素类型大小，比较函数的函数指针
     qsort(a, n, sizeof(STUDENTS), compare);
 }
 
 
-
+/// @return 返回值为数据条目的数量
 int getRecs(STUDENTS s[]) {
+    //以只读打开文件
     FILE* fp = fopen("records.txt", "r");
     int i;
+    //获取数据 当到达文件结尾或达到缓存数组上限时停止
     for (i = 0; !feof(fp) && i < 20; i++) {
         fscanf(fp, "%d %f", &(s[i].xh), &(s[i].sg));
     }
 
     return i;
 }
-
-
